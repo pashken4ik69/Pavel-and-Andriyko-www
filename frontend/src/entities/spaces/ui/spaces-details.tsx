cfb63d294c69ec4afc9cc33116c3bb76fb201a41
Salokihn Pavel <fpro36533@gmail.com>
2026-04-28 13:24:17 +0300

    test

--- frontend/src/entities/spaces/ui/spaces-details.tsx
+++ frontend/src/entities/spaces/ui/spaces-details.tsx
@@ -2,33 +2,32 @@ import { useNavigate, useParams } from "react-router";
 import { useGetSpaceQuery } from "../api/spces-api";
 import { useAppSelector } from "../../../app/store/store";
 function SpacesDetails() {
-    const { id } = useParams<{ id: string }>();
-    const navigate = useNavigate()
-    const { data: space } = useGetSpaceQuery(id || "");
-    const { user } = useAppSelector((state) => state.auth);
+  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+  const { data: space } = useGetSpaceQuery(id || "");
+  const { user } = useAppSelector((state) => state.auth);
 
+  if (!space) return <div>Данные отсутствуют</div>;
 
-    if (!space) return <div>Данные отсутствуют</div>;
-
-    return (
-        <div className="details">
-            <img src={space.images} alt={space.images} />
-            <h3>{space.title}</h3>
-            <p>{space.description}</p>
-            <p>{space.capacity}</p>
-            <p>{space.pricePerHour}</p>
-            <p>{space.rating}</p>
-            <p>{space.zoneType}</p>
-            <button onClick={() => navigate("/spaces")}>Назад</button>
-            <button>Забронировать</button>
-            {user?.role === "manager" && (
-                <>
-                    <button>Удалить</button>
-                    <button>Изменить</button>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="details">
+      <img src={space.images} alt={space.images} />
+      <h3>{space.title}</h3>
+      <p>{space.description}</p>
+      <p>{space.capacity}</p>
+      <p>{space.pricePerHour}</p>
+      <p>{space.rating}</p>
+      <p>{space.zoneType}</p>
+      <button onClick={() => navigate("/spaces")}>Назад</button>
+      <button>Забронировать</button>
+      {user?.role === "manager" && (
+        <>
+          <button>Удалить</button>
+          <button>Изменить</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default SpacesDetails;
